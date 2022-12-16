08d99c4e7e82c99d600a15eacd6354739c65a7a4
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2022-12-16 19:19:23 +0100

    Add modular App project with main module package

---
+++ modern/ModularApp/MainModule/Sources/MainModule/MainModule.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+public struct MainModule {
+    public private(set) var text = "Hello, World!"
+    
+    public var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+
+    public init() {
+    }
+}
---
+++ modern/ModularApp/ModularApp/ModularAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  ModularAppApp.swift
+//  ModularApp
+//
+//  Created by Kanstantsin Bucha on 16/12/2022.
+//
+
+import SwiftUI
+import MainModule
+
+@main
+struct ModularAppApp: App {
+    private let main = MainModule()
+    
+    var body: some Scene { main.body }
+}
