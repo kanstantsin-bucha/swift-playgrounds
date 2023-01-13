4f8d5523975ac67fc86c5570feb37e6169d90336
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2023-01-13 14:37:50 +0100

    add text field with double side binding to Double in number and currency formats

--- modern/_100DaysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
+++ modern/_100DaysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -8,16 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let users = ["Mike", "Chack", "Bob"]
-    @State private var admin = "Mike"
+    @State private var checkAmount = 10.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    private var format: FloatingPointFormatStyle<Double>.Currency {
+        .currency(code: Locale.current.currencyCode ?? "USD")
+    }
+    
+    private let tipPercentages = [10, 15, 20, 25, 0]
     var body: some View {
         Form {
-            Picker("admin", selection: $admin) {
-                ForEach(users, id: \.self) {
-                    Text("\($0)")
+            Section {
+                TextField(
+                    "Check amount",
+                    value: $checkAmount,
+                    format: .number
+                )
+                .keyboardType(.decimalPad)
+                .onTapGesture {
+                    checkAmount = 0.0
                 }
             }
-            Text("admin is \(admin)")
+            Section {
+                Text(checkAmount, format: format)
+            }
         }
     }
 }
