3ce7196f89c42579ae8875ee871493eff1363564
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2023-01-16 09:03:42 +0100

    rename playground

--- modern/_100DaysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ modern/_100DaysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isAnimated = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(
+                gradient:
+                    Gradient(stops: [
+                        .init(color: .blue, location: 0.01),
+                        .init(color: .black, location: 0.30),
+                        .init(color: .white, location: 0.65),
+                        .init(color: .gray, location: 0.8),
+                    ]),
+                center: .center,
+                startRadius: 0,
+                endRadius: 250
+            )
+            RadialGradient(
+                gradient:
+                    Gradient(stops: [
+                        .init(color: .blue, location: 0.01),
+                        .init(color: .black, location: 0.3),
+                        .init(color: .white, location: 0.65),
+                        .init(color: .gray, location: 0.8),
+                    ]),
+                center: .center,
+                startRadius: 0,
+                endRadius: 250
+            )
+            .opacity(isAnimated ? 1 : 0)
+            RadialGradient(
+                gradient:
+                    Gradient(stops: [
+                        .init(color: .blue, location: 0.03),
+                        .init(color: .black, location: 0.43),
+                        .init(color: .white, location: 0.72),
+                        .init(color: .gray, location: 0.8),
+                    ]),
+                center: .center,
+                startRadius: 0,
+                endRadius: 250
+            )
+            .opacity(isAnimated ? 0 : 1)
         }
-        .padding()
+//        .frame(width: isAnimated ? 300 : 330, height: isAnimated ? 300 : 330, alignment: .center)
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 1).repeatCount(10)) {
+                isAnimated.toggle()
+            }
+        }
+        .ignoresSafeArea(.all)
     }
 }
 
