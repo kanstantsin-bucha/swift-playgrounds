1213f6a70f4efdd410856a530579fb4c994b0391
alexandr bucha <aliaksandr.bucha@gmail.com>
2023-03-10 15:09:26 +0100

    Add SwiftPackageView

---
+++ _100DaysOfSwiftUI/100DaysOverview/100Days/SwiftPackageView.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftPackageView.swift
+//  100Days
+//
+//  Created by Aliaksandr Bucha on 10/03/2023.
+//
+
+import SwiftUI
+
+//This example without Package
+//Show only conversion [Int] in String
+struct SwiftPackageView: View {
+    let numbers = Array(1...10)
+    var body: some View {
+        Text(results)
+    }
+    
+    var results: String {
+        return numbers.map(String.init).joined(separator: ", ")
+    }
+}
+
+struct SwiftPackageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftPackageView()
+    }
+}
