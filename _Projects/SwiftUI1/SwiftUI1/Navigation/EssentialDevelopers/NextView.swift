c1545cecb17b4606e63ec2e90c4d0ce6946b9a61
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2023-03-26 15:08:28 +0200

    Add the `Next` view for the deep hierarchy navigation.

---
+++ _Projects/SwiftUI1/SwiftUI1/Navigation/EssentialDevelopers/NextView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+public class NextViewModel: ObservableObject {
+    @Published var isActive = false
+    
+    public func activate() {
+        isActive = true
+    }
+}
+
+public struct NextView<Content: View>: View {
+    // Let's check if binding works here ?
+    @ObservedObject private var model: NextViewModel
+    let content: () -> Content
+    
+    public init(model: NextViewModel, content: @escaping () -> Content) {
+        self.model = model
+        self.content = content
+    }
+    
+    public var body: some View {
+        NavigationLink(
+            isActive: $model.isActive,
+            destination: content,
+            label: EmptyView.init
+        )
+    }
+}
+
+
+struct NextView_Previews: PreviewProvider {
+    static var previews: some View {
+        NextView()
+    }
+}
