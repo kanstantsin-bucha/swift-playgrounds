f6099a525b0bde841446eeea2d49b1d25d5d8298
alexandr bucha <aliaksandr.bucha@gmail.com>
2023-04-19 09:06:24 +0200

    Add NotifiedBackgroundView

--- _100DaysOfSwiftUI/100DaysOverview/100Days/NotifiedBackgroundView.swift
+++ _100DaysOfSwiftUI/100DaysOverview/100Days/NotifiedBackgroundView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct NotifiedBackgroundView: View {
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    print("Active")
+                } else if  newPhase == . inactive {
+                    print("Inactive")
+                } else if newPhase == .background {
+                    print("Background")
+                }
+            }
     }
 }
 
