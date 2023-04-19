137ad887c075a26dc8de3dcba977cc8bf9275c03
alexandr bucha <aliaksandr.bucha@gmail.com>
2023-04-19 09:06:55 +0200

    Add AccessibilityView

--- _100DaysOfSwiftUI/100DaysOverview/100Days/AccessibilityView.swift
+++ _100DaysOfSwiftUI/100DaysOverview/100Days/AccessibilityView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct AccessibilityView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if differentiateWithoutColor {
+                Image(systemName: "checkmark.circle")
+            }
+            Text("Success")
+                .padding()
+                .background(differentiateWithoutColor ? .black : .green)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+        }
     }
 }
 
