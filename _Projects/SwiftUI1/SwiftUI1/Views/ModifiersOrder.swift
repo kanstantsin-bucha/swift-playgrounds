262700b75829d40739424d852b4e84868d7ca853
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2023-06-11 13:34:17 +0200

    Add an example that demonstrates the order of appliing modifiers.

---
+++ _Projects/SwiftUI1/SwiftUI1/Views/ModifiersOrder.swift
@@ -0,0 +1,23 @@
+//
+//  ModifiersOrder.swift
+//  SwiftUI1
+//
+//  Created by Kanstantsin Bucha on 07/04/2023.
+//
+
+import SwiftUI
+
+struct ModifiersOrder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(.red)
+            .padding(16)
+            .background(.blue)
+    }
+}
+
+struct ModifiersOrder_Previews: PreviewProvider {
+    static var previews: some View {
+        ModifiersOrder()
+    }
+}
